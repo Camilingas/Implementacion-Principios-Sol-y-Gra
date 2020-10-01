37764107bf388bd8e95e3f5bae618099cb88d907
Camilingas <71770465+Camilingas@users.noreply.github.com>
2020-09-30 20:25:40 -0500

    Add files via upload

---
+++ Principio de Responsabilidad Unica python.py
@@ -0,0 +1,11 @@
+Class Animales{
+    String raza;
+Class Animales
+Animales(String raza) {This.raza=raza;}
+String getRazaAnimales(){return raza; } 
+} 
+Class AnimalesDB{
+   Void guardar AnimalDB(Animales Animales){...} 
+   void eliminar Animales(Animales Animales){...} 
+
+
